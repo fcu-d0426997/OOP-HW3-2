b52e942612a3ea372f6472be94b506ef542bd76f
zack <zack@zack-pc>
2016-10-27 21:06:49 +0800

    HW1:let it go

---
+++ src/fcu/iecs/oop/Hello.java
@@ -0,0 +1,19 @@
+package fcu.iecs.oop;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+		
+		String lyrics2 = lyrics.replace("it", "her");
+		
+		String lyrics3 = lyrics2.replace("Cannot", "Can't");
+		
+		System.out.println(lyrics3);
+		
+				
+	}
+
+}
